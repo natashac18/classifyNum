207efc57efb587f7f890440da931a9ef206302fb
Tashalove18 <tashachitongo@gmail.com>
2021-06-17 16:50:07 +0200

    placed border around the button

--- main.js
+++ main.js
@@ -3,9 +3,10 @@ function classifyNum(){
     let num = prompt("Enter a number");
     let test= num % 2;
 
+
     if (test===0){
         document.getElementById("Answer").innerHTML= "The number " + num + " is EVEN!"
-    } else{
+    } else {
         document.getElementById("Answer").innerHTML= "The number " + num + " is ODD!"
-    }
+    }  
 }
