357c9f6d9e918ad351507e034c63d6293d3fa8a7
Tashalove18 <tashachitongo@gmail.com>
2021-06-17 16:26:28 +0200

    created an odd/even function

---
+++ main.js
@@ -0,0 +1,11 @@
+
+function classifyNum(){
+    let num = prompt("Enter a number");
+    let test= num % 2;
+
+    if (test===0){
+        document.getElementById("Answer").innerHTML= "The number " + num + " is EVEN!"
+    } else{
+        document.getElementById("Answer").innerHTML= "The number " + num + " is ODD!"
+    }
+}
